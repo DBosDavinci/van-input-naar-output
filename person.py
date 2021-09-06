bbe070709248ed8742c836a32bee7a768d7b61d3
DBosDavinci <99069008@mydavinci.nl>
2021-09-06 16:04:28 +0200

    person

---
+++ person.py
@@ -0,0 +1,11 @@
+Name = input('Wat is uw naam?')
+Address = input('Wat is uw adres?')
+Code = input('Wat is uw postcode?')
+Place = input('Wat is uw woonplaats?')
+
+print(' ----------------------------------------------------\n'
+'|  Naam      :', Name,
+'\n|  Adres     :', Address,
+'\n|  Postcode  :', Code,
+'\n|  Woonplaats:', Place,
+'\n ----------------------------------------------------')
